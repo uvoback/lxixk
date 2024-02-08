19c166ff6fb8bcf00593749278472a43a4ed0c14
xtidozy <xtidozy@outlook.com>
2024-02-08 22:08:35 +0800

    '---'

---
+++ min/v.js
@@ -0,0 +1,23 @@
+if (neatSrc) neatSrc.push({
+    domainUrl: "https://lik.dozy.eu.org",
+    backgrounds: [
+    ],
+    music: [
+        {id: "Color_Code_F", name: sc.ep, author: "フェルト"},
+        {id: "Clan3yang0yang0", name: sc.lg+"#懒羊羊", author: "Taylor Swift"},
+        {id: "Cping2fan2zhi1lu4", name: sc.lg+"#平凡之路", author: "朴树"},
+        {id: "Juice_World", name: sc.ra},
+        {id: "ONE_PUNCH_KO", name: sc.sp},
+        {id: "Climb_Ka", name: sc.ra},
+        {id: "A", name: sc.ms},
+        {id: "Others_i_World"},
+        {id: "The_Nights_Ever_Fading", name: sc.sp, author: "DInxm Robinson"},
+        {id: "I_Walk_This_Earth_All_By_Myself", name: sc.ra, author: "EKKSTACY".ank(sc.ep)},
+        {id: "A4KEY", name: sc.ra},
+        {id: "Lava_Drinker", name: sc.ot},
+        {id: "Cxi3huan1he2ni3de1xia4tian1", name: sc.ep+"#喜欢和你的夏天"},
+        {id: "Fatal_Love", name: sc.lg, author: "Jori King"},
+        {id: "POLICMR_LKW_SERGEAN", name: sc.ep},
+        {id: "Cxia4ge4lu4kou3zai4jian4ba1", name: sc.ep+"#下个路口再见吧"},
+    ]
+});
